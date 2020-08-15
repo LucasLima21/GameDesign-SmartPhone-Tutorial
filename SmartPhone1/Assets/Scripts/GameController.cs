4912928080fbb2e47fd33641416a7f8e4cc188f2
LucasLima21 <ldsllm.eng@uea.edu.br>
2020-08-15 02:44:35 -0300

    Tutorial de jogo em android com unity

---
+++ SmartPhone1/Assets/Scripts/GameController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class GameController : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+    public Text timerDisplay;
+    public float timer;
+    private bool asLost;
+
+    private int sentinel = 0;
+    void Update(){ 
+        if(asLost == false){
+            timerDisplay.text = timer.ToString("F0");
+        }
+
+        if(timer <= 0){
+            sentinel++;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            if(sentinel == 5){
+                gameOverPanel.SetActive(true); //quando finaliza a fase 5 é o fim
+            }
+        }
+        else{
+            timer -= Time.deltaTime;
+        }
+    }
+    
+    /*
+    void Delay(){
+        gameOverPanel.SetActive(true);
+    }*/
+    public void ShowGameOver(){
+        asLost = true;
+        gameOverPanel.SetActive(true);
+        
+    }
+    public void GoToScene(){
+        SceneManager.LoadScene("Level_1");
+    }
+
+    public void RestartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void GoToMainMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+}
---
+++ SmartPhone1/Assets/Scripts/RandomBall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class RandomBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float minX, minY, maxX, maxY, minSpeed, maxSpeed, secondsMaxToDifficulty;
+    float speed;
+    Vector2 targetPosition;
+    void Start()
+    {
+        targetPosition = GetRandomPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((Vector2)transform.position != targetPosition){
+            speed = Mathf.Lerp(minSpeed, maxSpeed, GetDifficultPercent());
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, (speed*Time.deltaTime));
+        }
+        else{
+            targetPosition = GetRandomPosition();
+        }
+    }
+
+    Vector2 GetRandomPosition(){
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+        return new Vector2(randomX, randomY);
+    }
+
+    float GetDifficultPercent(){
+        return Mathf.Clamp01(Time.timeSinceLevelLoad/secondsMaxToDifficulty);
+    }
+}
